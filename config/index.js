bc7d99973f2e507944eae32e80085ee70ae194b2
changjinglong <157036216+changjinglong@users.noreply.github.com>
2024-01-18 23:05:30 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,29 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
+  appid: "wxfe3fda0265567ca4",
+  appsecret: "3031a80904e98faea61e44d3c2f890d4",
+  template_id: "ooHyC6b_56TlWxmuCenkrsMI2BcM",
   touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
   city: "邢台",
-  first_date: "2022-11-10",
+  first_date: "2020-07-19",
 };
+💓可爱的：{{name.阳阳老婆}} 
+📅今天是：{{date.DATA}} 
+🌍地区：{{city.固安}} 
+☁️今日天气：{{weather.DATA}} 
+🌡️当前温度：{{now_temperature.DATA}} 
+🌕今日最低温度：{{min_temperature.DATA}}
+🌞今日最高温度：{{max_temperature.DATA}}  
+❤️我们已经恋爱：{{love_date.DATA}}  
+🎂距离你这个小笨蛋的生日还有：{{birthday.DATA}}  
+🍰距离我的生日还有：{{my_birthday.DATA}}  
+🏮距离春节还有：{{diff_date1.DATA}}
+🏮距离考研还有：{{diff_date2.DATA}}
+🌄日出时间：{{sunrise.DATA}}
+🌄日落时间：{{sunset.DATA}}
+🌛夜间天气：{{textNight.DATA}}
+☁️白天风向：{{windDirDay.DATA}}
+☁️夜间风向：{{windDirNight.DATA}}
+🐝风力等级：{{windScaleDay.DATA}}
+💬{{note.DATA}}
